22d191299fce8ae267ad7891b58676b203ec6108
Georgiy Korneev <kgeorgiy@kgeorgiy.info>
2023-03-06 12:46:13 +0300

    + Example JS solution

---
+++ javascript-solutions/example.js
@@ -0,0 +1,11 @@
+const add = (a, b) => a + b;
+
+function hello(name) {
+    const message = `Hello, ${name}!`;
+    println("        " + message);
+    return message;
+}
+
+function checkStrict() {
+    UNDEFINED = "value";
+}
