ceb2322865135d4dda5d08bee4a2ca007aa04c22
Fedor Bondarev <fedorbondarev@outlook.com>
2023-03-23 15:27:05 +0300

    Fixed solution for 6 homework

--- javascript-solutions/functionalExpression.js
+++ javascript-solutions/functionalExpression.js
@@ -1,44 +1,61 @@
-const cnst = (n) => () => Number(n);
+const cnst = (a) => (() => a);
+const variable = (v) => ((x, y, z) => ({x, y, z})[v]);
 
-const variable = (v) => () => eval(v);
+const negate = (expr) => ((...args) => -expr(...args))
 
-const buildOperationExpression =
+const buildBinaryExpression =
     (operation) =>
         (expr1, expr2) =>
             (...args) =>
                 operation(expr1(...args), expr2(...args));
 
-const add = buildOperationExpression((a, b) => a + b);
-const subtract = buildOperationExpression((a, b) => a + b);
-const multiply = buildOperationExpression((a, b) => a * b);
-const divide = buildOperationExpression((a, b) => a / b);
+const add = buildBinaryExpression((a, b) => a + b);
+const subtract = buildBinaryExpression((a, b) => a - b);
+const multiply = buildBinaryExpression((a, b) => a * b);
+const divide = buildBinaryExpression((a, b) => a / b);
 
 const parse = (s) => {
     let expressions = [];
 
-    for (const element of s.split(" ")) {
-        if (["+", "-", "*", "/"].includes(element)) {
+    let t = "";
+
+    for (let i = 0; i < s.length; i++) {
+        let ch = s[i];
+
+        if (/\s/.test(ch)) continue;
+
+        if (
+            /\d|\./.test(ch)
+            || (ch === "-" && /\d|\./.test(s[i + 1]))
+        ) {
+            t += ch;
+            if (!/\d|\./.test(s[i + 1])) {
+                let n = Number(t);
+                if (!isNaN(n)) {
+                    expressions.push(cnst(n));
+                    t = "";
+                }
+            }
+        } else if (["+", "-", "*", "/"].includes(ch)) {
             let expr2 = expressions.pop();
             let expr1 = expressions.pop();
 
-            switch (element) {
-                case "+":
-                    expressions.push(add(expr1, expr2));
-                    break;
-                case "-":
-                    expressions.push(subtract(expr1, expr2));
-                    break;
-                case "*":
-                    expressions.push(multiply(expr1, expr2));
-                    break;
-                case "/":
-                    expressions.push(divide(expr1, expr2));
-                    break;
+            expressions.push(
+                (
+                    ({
+                        "+": add,
+                        "-": subtract,
+                        "*": multiply,
+                        "/": divide
+                    })[ch]
+                )(expr1, expr2)
+            )
+        } else if (["x", "y", "z"].includes(ch)) {
+            expressions.push(variable(ch));
+        } else if (ch === "n") {
+            if (s.substring(i, i + 6) === "negate") {
+                expressions.push(negate(expressions.pop()));
             }
-        } else if (["x", "y", "z"].includes(element)) {
-            expressions.push(variable(element));
-        } else {
-            expressions.push(cnst(Number(element)));
         }
     }
 
@@ -46,14 +63,15 @@ const parse = (s) => {
 };
 
 // Test x^2 - 2x + 1
-// let myExpr = add(
-//   subtract(
-//     multiply(variable("x"), variable("x")),
-//     multiply(cnst(2), variable("x"))
-//   ),
-//   cnst(1)
-// );
 
-// for (const x of [...Array(11).keys()]) {
-//   println(myExpr(x));
-// }
+let myExpr = add(
+    subtract(
+        multiply(variable("x"), variable("x")),
+        multiply(cnst(2), variable("x"))
+    ),
+    cnst(1)
+);
+
+for (const x of [...Array(11).keys()]) {
+    println(myExpr(x));
+}
