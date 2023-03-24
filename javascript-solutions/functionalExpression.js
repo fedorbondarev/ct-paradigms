63c0be257f9289550a3159cc0117b7b10e38e7b0
Fedor Bondarev <fedorbondarev@outlook.com>
2023-03-24 16:00:19 +0300

    Added 6 homework modification

--- javascript-solutions/functionalExpression.js
+++ javascript-solutions/functionalExpression.js
@@ -1,66 +1,50 @@
+"use strict";
+
 const cnst = (a) => (() => a);
 const variable = (v) => ((x, y, z) => ({x, y, z})[v]);
 
-const negate = (expr) => ((...args) => -expr(...args))
-
-const buildBinaryExpression =
+const buildExpression =
     (operation) =>
-        (expr1, expr2) =>
-            (...args) =>
-                operation(expr1(...args), expr2(...args));
+        (...expressions) =>
+            (...args) => operation(...expressions.map((expr) => expr(...args)));
 
-const add = buildBinaryExpression((a, b) => a + b);
-const subtract = buildBinaryExpression((a, b) => a - b);
-const multiply = buildBinaryExpression((a, b) => a * b);
-const divide = buildBinaryExpression((a, b) => a / b);
+const add = buildExpression((a, b) => a + b);
+const subtract = buildExpression((a, b) => a - b);
+const multiply = buildExpression((a, b) => a * b);
+const divide = buildExpression((a, b) => a / b);
+
+const negate = buildExpression((a) => -a);
+const sin = buildExpression((a) => Math.sin(a));
+const cos = buildExpression((a) => Math.cos(a));
+
+const one = cnst(1)
+const two = cnst(2)
 
 const parse = (s) => {
-    let expressions = [];
+    let binaryExpressions = {"+": add, "-": subtract, "*": multiply, "/": divide,};
+    let unaryExpressions = {negate, sin, cos};
+    let constants = {one, two};
 
-    let t = "";
+    let elements = [];
 
-    for (let i = 0; i < s.length; i++) {
-        let ch = s[i];
-
-        if (/\s/.test(ch)) continue;
-
-        if (
-            /\d|\./.test(ch)
-            || (ch === "-" && /\d|\./.test(s[i + 1]))
-        ) {
-            t += ch;
-            if (!/\d|\./.test(s[i + 1])) {
-                let n = Number(t);
-                if (!isNaN(n)) {
-                    expressions.push(cnst(n));
-                    t = "";
-                }
-            }
-        } else if (["+", "-", "*", "/"].includes(ch)) {
-            let expr2 = expressions.pop();
-            let expr1 = expressions.pop();
-
-            expressions.push(
-                (
-                    ({
-                        "+": add,
-                        "-": subtract,
-                        "*": multiply,
-                        "/": divide
-                    })[ch]
-                )(expr1, expr2)
-            )
-        } else if (["x", "y", "z"].includes(ch)) {
-            expressions.push(variable(ch));
-        } else if (ch === "n") {
-            if (s.substring(i, i + 6) === "negate") {
-                expressions.push(negate(expressions.pop()));
-            }
+    for (const element of s.split(/\s+/g)) {
+        if (binaryExpressions[element] !== undefined) {
+            let expr2 = elements.pop();
+            let expr1 = elements.pop();
+            elements.push(binaryExpressions[element](expr1, expr2))
+        } else if (unaryExpressions[element] !== undefined) {
+            elements.push(unaryExpressions[element](elements.pop()));
+        } else if (constants[element] !== undefined) {
+            elements.push(constants[element]);
+        } else if (["x", "y", "z"].includes(element)) {
+            elements.push(variable(element));
+        } else if (element.length !== 0) {
+            elements.push(cnst(Number(element)))
         }
     }
 
-    return expressions[expressions.length - 1];
-};
+    return elements[elements.length - 1]
+}
 
 // Test x^2 - 2x + 1
 
@@ -68,8 +52,7 @@ let myExpr = add(
     subtract(
         multiply(variable("x"), variable("x")),
         multiply(cnst(2), variable("x"))
-    ),
-    cnst(1)
+    ), cnst(1)
 );
 
 for (const x of [...Array(11).keys()]) {
