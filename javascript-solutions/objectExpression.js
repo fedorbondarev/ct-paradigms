0e8fdb799d76cea0432b65c523c0a861ee4b4380
Fedor Bondarev <fedorbondarev@outlook.com>
2023-04-13 16:19:36 +0300

    Modified 7 homework

--- javascript-solutions/objectExpression.js
+++ javascript-solutions/objectExpression.js
@@ -2,6 +2,7 @@
 
 "use strict";
 
+
 function Expression(...childExpressions) {
     this._childExpressions = childExpressions;
 }
@@ -29,6 +30,7 @@ Expression.prototype.diff = function (diffVariable) {
     )
 }
 
+
 function Variable(name) {
     this.evaluate = (x, y, z) => ({x, y, z}[name]);
     this.toString = () => name;
@@ -41,92 +43,84 @@ function Const(value) {
     this.diff = () => new Const(0);
 }
 
-function Add(expr1, expr2) {
-    Expression.call(this, expr1, expr2);
+
+let operators = {}
+
+function buildComplexExpression(apply, operator, length, applyDiff) {
+    function NewExpression(...args) {
+        Expression.call(this, ...args);
+    }
+
+    NewExpression.prototype = Object.create(Expression.prototype);
+
+    NewExpression.prototype._apply = apply;
+    NewExpression.prototype.operator = operator;
+    NewExpression.prototype.length = length;
+
+    NewExpression.prototype._applyDiff = applyDiff;
+
+    operators[operator] = NewExpression;
+
+    return NewExpression;
 }
 
-Add.prototype = Object.create(Expression.prototype);
+let Add = buildComplexExpression(
+    (a, b) => a + b,
+    "+",
+    2,
+    (expr1, expr2) => new Add(expr1.diffValue, expr2.diffValue),
+)
 
-Add.prototype._apply = (a, b) => a + b;
-Add.prototype.operator = "+";
-Add.prototype._applyDiff = (expr1, expr2) => new Add(expr1.diffValue, expr2.diffValue);
+let Subtract = buildComplexExpression(
+    (a, b) => a - b,
+    "-",
+    2,
+    (expr1, expr2) => new Subtract(expr1.diffValue, expr2.diffValue),
+)
 
-function Subtract(expr1, expr2) {
-    Expression.call(this, expr1, expr2);
-}
-
-Subtract.prototype = Object.create(Expression.prototype);
-
-Subtract.prototype._apply = (a, b) => a - b;
-Subtract.prototype.operator = "-";
-Subtract.prototype._applyDiff = (expr1, expr2) => new Subtract(expr1.diffValue, expr2.diffValue);
-
-function Multiply(expr1, expr2) {
-    Expression.call(this, expr1, expr2);
-}
-
-Multiply.prototype = Object.create(Expression.prototype);
-
-Multiply.prototype._apply = (a, b) => a * b;
-Multiply.prototype.operator = "*";
-Multiply.prototype._applyDiff = (expr1, expr2) => new Add(
-    new Multiply(expr1.diffValue, expr2.value),
-    new Multiply(expr2.diffValue, expr1.value),
-);
-
-
-function Divide(expr1, expr2) {
-    Expression.call(this, expr1, expr2);
-}
-
-Divide.prototype = Object.create(Expression.prototype);
-
-Divide.prototype._apply = (a, b) => a / b;
-Divide.prototype.operator = "/";
-Divide.prototype._applyDiff = (expr1, expr2) => new Divide(
-    new Subtract(
+let Multiply = buildComplexExpression(
+    (a, b) => a * b,
+    "*",
+    2,
+    (expr1, expr2) => new Add(
         new Multiply(expr1.diffValue, expr2.value),
         new Multiply(expr2.diffValue, expr1.value),
     ),
-    new Multiply(expr2.value, expr2.value)
-);
+)
 
-function Negate(expr) {
-    Expression.call(this, expr);
-}
+let Divide = buildComplexExpression(
+    (a, b) => a / b,
+    "/",
+    2,
+    (expr1, expr2) => new Divide(
+        new Subtract(
+            new Multiply(expr1.diffValue, expr2.value),
+            new Multiply(expr2.diffValue, expr1.value),
+        ),
+        new Multiply(expr2.value, expr2.value)
+    ),
+)
 
-Negate.prototype = Object.create(Expression.prototype);
+let Negate = buildComplexExpression(
+    a => -a,
+    "negate",
+    1,
+    (expr) => new Negate(expr.diffValue),
+)
 
-Negate.prototype._apply = a => -a;
-Negate.prototype.operator = "negate";
-Negate.prototype._applyDiff = (expr) => new Negate(expr.diffValue);
+let Exp = buildComplexExpression(
+    a => Math.exp(a),
+    "exp",
+    1,
+    (expr) => new Multiply(new Exp(expr.value), expr.diffValue),
+)
 
-function Exp(expr) {
-    Expression.call(this, expr);
-}
-
-Exp.prototype = Object.create(Expression.prototype);
-
-Exp.prototype._apply = a => Math.exp(a);
-Exp.prototype.operator = "exp";
-Exp.prototype._applyDiff = (expr) => new Multiply(new Exp(expr.value), expr.diffValue);
-
-function Ln(expr) {
-    Expression.call(this, expr);
-}
-
-Ln.prototype = Object.create(Expression.prototype);
-
-Ln.prototype._apply = a => Math.log(a);
-Ln.prototype.operator = "ln";
-Ln.prototype._applyDiff = (expr) => new Multiply(new Divide(new Const(1), expr.value), expr.diffValue);
-
-let expressions = [Variable, Const, Add, Subtract, Multiply, Divide, Negate, Exp, Ln]
-let operators = {}
-
-for (const expression of expressions) {
-    operators[expression.prototype.operator] = expression;
-}
+let Ln = buildComplexExpression(
+    a => Math.log(a),
+    "ln",
+    1,
+    (expr) => new Multiply(new Divide(new Const(1), expr.value), expr.diffValue),
+)
 
 function parse(s) {
     let elements = [], elementString = "";
@@ -135,7 +129,9 @@ function parse(s) {
             if (operators[elementString] !== undefined) {
                 let expression = operators[elementString];
                 elements.push(new expression(
-                    ...Array.from({length: expression.length}, () => elements.pop()).reverse()
+                    ...Array.from(
+                        {length: expression.prototype.length}, () => elements.pop()
+                    ).reverse()
                 ));
             } else if (["x", "y", "z"].includes(elementString)) {
                 elements.push(new Variable(elementString));
