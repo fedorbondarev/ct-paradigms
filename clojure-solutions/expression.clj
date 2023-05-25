5c1eb9392cc9a11129409f8def0faccc345dca67
Fedor Bondarev <fedorbondarev@outlook.com>
2023-05-25 17:12:20 +0300

    Added solution for 12 homework

--- clojure-solutions/expression.clj
+++ clojure-solutions/expression.clj
@@ -83,6 +83,7 @@
 
 (definterface IExpression
   (^Number evaluate [vars])
+  (^String toStringPostfix [])
   ;(^Number diff [diffVarName])
   )
 
@@ -91,6 +92,7 @@
   (toString [_this] (str value))
   IExpression
   (evaluate [_this _vars] value)
+  (toStringPostfix [this] (.toString this))
   ;(diff [_this _diffVarName] (_Constant. 0))
   )
 
@@ -98,22 +100,38 @@
   Object
   (toString [_this] name)
   IExpression
-  (evaluate [_this vars] (get vars name))
+  (evaluate [_this vars]
+    (get
+      vars
+      (clojure.string/lower-case
+        (str (get name 0)))))
+  (toStringPostfix [this] (.toString this))
   ;(diff [_this diffVarName]
   ;  (if (= diffVarName name)
   ;    (_Constant. 1)
   ;    (_Constant. 0)))
   )
 
+(declare toString)
+(declare toStringPostfix)
+
 (defn evaluateAndApply [expressions vars fun]
   (apply fun
          (mapv #(.evaluate % vars) expressions)))
 
+(defn exprListToStr [exprs elementToStr]
+  (clojure.string/join " "
+                       (mapv elementToStr exprs)))
+
 (defn expressionToString [operator children]
   (str "(" operator " "
-       (clojure.string/join " "
-                            (mapv #(.toString %) children))
+       (exprListToStr children toString)
        ")"))
+
+(defn expressionToStringPostfix [operator children]
+  (str "("
+       (exprListToStr children toStringPostfix)
+       " " operator ")"))
 
 (defn applyDiff [children diffVarName diffRule]
   (diffRule children
@@ -138,34 +156,40 @@
       (diffRule f g f' g')
       )))
 
+(def _addSymbol "+")
 (deftype _Add [expressions]
   Object
-  (toString [_this] (expressionToString "+" expressions))
+  (toString [_this] (expressionToString _addSymbol expressions))
   IExpression
   (evaluate [_this vars]
     (evaluateAndApply expressions vars +))
+  (toStringPostfix [_this] (expressionToStringPostfix _addSymbol expressions))
   ;(diff [_this diffVarName]
   ;  (applyDiff expressions diffVarName
   ;             (fn [_exprs diffExprs] (_Add. diffExprs))))
   )
 
+(def _subtractSymbol "-")
 (deftype _Subtract [expressions]
   Object
-  (toString [_this] (expressionToString "-" expressions))
+  (toString [_this] (expressionToString _subtractSymbol expressions))
   IExpression
   (evaluate [_this vars]
     (evaluateAndApply expressions vars -))
+  (toStringPostfix [_this] (expressionToStringPostfix _subtractSymbol expressions))
   ;(diff [_this diffVarName]
   ;  (applyDiff expressions diffVarName
   ;             (fn [_exprs diffExprs] (_Subtract. diffExprs))))
   )
 
+(def _multiplySymbol "*")
 (deftype _Multiply [expressions]
   Object
-  (toString [_this] (expressionToString "*" expressions))
+  (toString [_this] (expressionToString _multiplySymbol expressions))
   IExpression
   (evaluate [_this vars]
     (evaluateAndApply expressions vars *))
+  (toStringPostfix [_this] (expressionToStringPostfix _multiplySymbol expressions))
   ;(diff [_this diffVarName]
   ;  (applyRightFoldDiff expressions diffVarName
   ;                      (fn [f g f' g']
@@ -175,12 +199,14 @@
   ;                      (fn [& args] (_Multiply. args))))
   )
 
+(def _divideSymbol "/")
 (deftype _Divide [expressions]
   Object
-  (toString [_this] (expressionToString "/" expressions))
+  (toString [_this] (expressionToString _divideSymbol expressions))
   IExpression
   (evaluate [_this vars]
     (evaluateAndApply expressions vars divideWithInfinity))
+  (toStringPostfix [_this] (expressionToStringPostfix _divideSymbol expressions))
   ;(diff [_this diffVarName]
   ;  (applyRightFoldDiff expressions diffVarName
   ;                      (fn [f g f' g']
@@ -192,32 +218,62 @@
   ;                      (fn [& args] (_Divide. args))))
   )
 
+(def _negateSymbol "negate")
 (deftype _Negate [expression]
   Object
-  (toString [_this] (expressionToString "negate" [expression]))
+  (toString [_this] (expressionToString _negateSymbol [expression]))
   IExpression
   (evaluate [_this vars]
     (- (.evaluate expression vars)))
+  (toStringPostfix [_this] (expressionToStringPostfix _negateSymbol [expression]))
   ;(diff [_this diffVarName]
   ;  (_Negate (.diff expression diffVarName)))
   )
 
+(def _sinSymbol "sin")
 (deftype _Sin [expression]
   Object
-  (toString [_this] (expressionToString "sin" [expression]))
+  (toString [_this] (expressionToString _sinSymbol [expression]))
   IExpression
   (evaluate [_this vars]
     (Math/sin (.evaluate expression vars)))
+  (toStringPostfix [_this] (expressionToStringPostfix _sinSymbol [expression]))
   ;(diff [_this diffVarName]
   ;  (_Cos. (.diff expression diffVarName)))
   )
 
+(def _cosSymbol "cos")
 (deftype _Cos [expression]
   Object
-  (toString [_this] (expressionToString "cos" [expression]))
+  (toString [_this] (expressionToString _cosSymbol [expression]))
   IExpression
   (evaluate [_this vars]
     (Math/cos (.evaluate expression vars)))
+  (toStringPostfix [_this] (expressionToStringPostfix _cosSymbol [expression]))
+  ;(diff [_this diffVarName]
+  ;  (_Negate. (_Sin. (.diff expression diffVarName))))
+  )
+
+(def _incSymbol "++")
+(deftype _Inc [expression]
+  Object
+  (toString [_this] (expressionToString _incSymbol [expression]))
+  IExpression
+  (evaluate [_this vars]
+    (+ (.evaluate expression vars) 1))
+  (toStringPostfix [_this] (expressionToStringPostfix _incSymbol [expression]))
+  ;(diff [_this diffVarName]
+  ;  (_Cos. (.diff expression diffVarName)))
+  )
+
+(def _decSymbol "--")
+(deftype _Dec [expression]
+  Object
+  (toString [_this] (expressionToString _decSymbol [expression]))
+  IExpression
+  (evaluate [_this vars]
+    (- (.evaluate expression vars) 1))
+  (toStringPostfix [_this] (expressionToStringPostfix _decSymbol [expression]))
   ;(diff [_this diffVarName]
   ;  (_Negate. (_Sin. (.diff expression diffVarName))))
   )
@@ -225,6 +281,7 @@
 (defn evaluate [expr vars] (.evaluate expr vars))
 (defn diff [expr vars] (.diff expr vars))
 (defn toString [expr] (.toString expr))
+(defn toStringPostfix [expr] (.toStringPostfix expr))
 (defn Variable [name] (_Variable. name))
 (defn Constant [value] (_Constant. value))
 (defn Add [& expressions] (_Add. expressions))
@@ -234,6 +291,8 @@
 (defn Negate [expression] (_Negate. expression))
 (defn Sin [expression] (_Sin. expression))
 (defn Cos [expression] (_Cos. expression))
+(defn Inc [expression] (_Inc. expression))
+(defn Dec [expression] (_Dec. expression))
 
 (def objectOperators {
                       '+      Add
@@ -244,6 +303,83 @@
 
                       'sin    Sin
                       'cos    Cos
+
+                      '++     Inc
+                      '--     Dec
                       })
 
 (def parseObject (createParser Constant Variable objectOperators))
+
+(load-file "parser.clj")
+
+(declare get*expr)
+
+(def operatorsKeys (sort-by (comp - count) (mapv str (keys objectOperators))))
+
+(defn sign [c tail]
+  (if (#{\- \+} c)
+    (cons c tail)
+    tail))
+
+(def *digitsStr (+str (+plus (+char "0123456789"))))
+
+(def *number
+  (+map
+    (comp Constant read-string)
+    (+str
+      (+seqf
+        sign
+        (+opt (+char "+-"))
+        (+map
+          #(if (nil? (second %)) (first %) (apply str %))
+          (+seq
+            *digitsStr
+            (+opt
+              (+str (+seq
+                      (+char ".")
+                      *digitsStr)))))))))
+
+(def *variable
+  (+map
+    Variable
+    (+str (+plus (+char "xyzXYZ")))))
+
+(defn +string [s]
+  (+map
+    (partial apply str)
+    (apply +seq
+           (mapv (comp +char str) s))))
+
+(def *operator
+  (+map
+    #((symbol %) objectOperators)
+    (apply
+      +or
+      (mapv
+        +string
+        operatorsKeys))))
+
+(def *space (+char " \t\n\r"))
+(def *ws (+ignore (+star *space)))
+
+(def *complexExpr
+  (+map #(apply (second %) (first %))
+        (+seq
+          (+ignore (+char "("))
+          (+star (delay (get*expr)))
+          *operator
+          *ws
+          (+ignore (+char ")"))
+          )))
+
+(def *expr
+  (+map
+    first
+    (+seq
+      *ws
+      (+or *number *variable *complexExpr)
+      *ws)))
+
+(defn get*expr [] *expr)
+
+(def parseObjectPostfix (+parser *expr))
